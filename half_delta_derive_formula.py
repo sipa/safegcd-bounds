304a98fc6cfc03a42ed941d7bd5816fcc5d9e28b
Pieter Wuille <pieter@wuille.net>
2021-01-11 18:45:57 -0800

    Add hddivsteps explanation

---
+++ half_delta_derive_formula.py
@@ -0,0 +1,57 @@
+import re
+import math
+
+# Maximum denominator to consider.
+MAX_DENOM = 2000
+
+# Only derive a formula for at least this many steps.
+MIN_STEPS = 51
+
+# Direction coefficient to use.
+COEF = 2.30352752220748858833059897712765371918755
+
+RE = re.compile("^(\d+) 0x([0-9a-f]+) ")
+
+# Minimal observed offset log(threshold)-steps/COEF.
+MIN = (1000, None)
+# Compute MINODD and MINEVEN from the data.
+with open("half_delta_output.txt") as f:
+    for line in f:
+        match = RE.match(line)
+        if match:
+            steps = int(match[1]) + 1
+            thresh = int(match[2], 16) + 1
+            lthresh = math.log2(thresh)
+            offset = lthresh - steps / COEF
+            if steps >= MIN_STEPS:
+                MIN = min(MIN, (offset, steps))
+
+# Find numerator/denominator approximation for COEF.
+NUMER = 0
+DENOM = 1
+DENOMCOEF = 10
+for denom in range(1, MAX_DENOM):
+    numer = int(math.ceil(COEF * denom))
+    coef = numer / denom
+    assert coef >= COEF
+    if coef < DENOMCOEF:
+        NUMER = numer
+        DENOM = denom
+        DENOMCOEF = coef
+
+# Find the actual formula, combining the fractional approximation with offsets.
+TERM = int(math.ceil(-NUMER*(MIN[0])))
+print("steps <= floor((%i * lthresh + %i) / %i)" % (NUMER, TERM, DENOM))
+
+# Verify the formula with exact integer arithmetic (slow!).
+with open("half_delta_output.txt") as f:
+    for line in f:
+        match = RE.match(line)
+        if match:
+            steps = int(match[1]) + 1
+            thresh = int(match[2], 16) + 1
+            if steps % 100 == 0:
+                print("Verifying for steps=%i" % steps)
+            if steps >= MIN_STEPS:
+                bits = (thresh**(2*NUMER) - 1).bit_length()
+                assert steps*DENOM - TERM <= bits
